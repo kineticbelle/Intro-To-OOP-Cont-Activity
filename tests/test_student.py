729d4cb578bd761b885c9c998428d3d7a8938333
Laura Castro <lauracastrotech@gmail.com>
2025-04-01 14:31:22 -0400

    passes class instantiation test

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,21 @@
 from school_schedule.student import Student
 
-# Write your tests here!
+def test_student_class_instantiation():
+    # arrange
+    name = "Quinn" 
+    grade = "junior"
+    classes = [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+    # act
+    student = Student(name, grade, classes)
+
+    # assert
+    assert student.name == name
+    assert student.grade == grade
+    assert student.classes == classes
